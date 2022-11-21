7cefb14af374eb8337c411a600a8803c0a24786e
YIP Chi Hong <21467358@life.hkbu.edu.hk>
2022-11-21 18:12:39 +0800

    Build a new MongoDB database and make connection

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://comp7980group:j5CAghoqKa5aGaaP5xQ0mJkkzAacUQ1mu4GnuBctOCut0BUx7SQveoatW5d9X2koOpeByWq9jslPACDbonHIzg%3D%3D@comp7980group.mongo.cosmos.azure.com:10255/?ssl=true';
+
+var db;
+
+MongoClient.connect(url, function (err, client) {
+  db = client.db('comp7980groupDB');
+  console.log("DB connected");
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
