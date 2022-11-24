e86edbf9add9ddc07758d6d5c2173572c0d1eff1
Angus YIP <itchyip@cityu.edu.hk>
2022-11-24 11:33:46 +0800

    Successfully built the insert record by POST

--- express/routes/index.js
+++ express/routes/index.js
@@ -2,18 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://comp7980group:j5CAghoqKa5aGaaP5xQ0mJkkzAacUQ1mu4GnuBctOCut0BUx7SQveoatW5d9X2koOpeByWq9jslPACDbonHIzg%3D%3D@comp7980group.mongo.cosmos.azure.com:10255/?ssl=true';
+var url = 'mongodb://comp7980group:j5CAghoqKa5aGaaP5xQ0mJkkzAacUQ1mu4GnuBctOCut0BUx7SQveoatW5d9X2koOpeByWq9jslPACDbonHIzg==@comp7980group.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@comp7980group@';
 
 var db;
 
 MongoClient.connect(url, function (err, client) {
-  db = client.db('comp7980groupDB');
+  db = client.db('assessmentsDB');
   console.log("DB connected");
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/api/assessments', async function (req, res) {
+
+  let result = await db.collection("assessments").insertOne(req.body);
+
+  if (!result) return res.status(404).send('Unable to find the requested resource!');
+
+  res.status(201).json({ id: result.insertedId });
+
 });
 
 module.exports = router;
